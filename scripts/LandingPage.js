1c26fd8899ba2147b908d9071af425e50a2cac58
Chandra0804 <chandrasaiteja0804@gmail.com>
2023-03-14 23:39:16 +0530

    Linked Login Page

---
+++ scripts/LandingPage.js
@@ -0,0 +1,16 @@
+setTimeout(() => {
+    $(document).ready(function () {
+        //Preloader
+        preloaderFadeOutTime = 550;
+        function hidePreloader() {
+            var preloader = $('.spinner-wrapper');
+            preloader.fadeOut(preloaderFadeOutTime);
+        }
+        hidePreloader();
+    });
+}, 1000);
+
+function loadLoginPage() {
+    // Load the login page into the current window
+    window.location.href = "../src/Login_SignUp.html";
+  }
