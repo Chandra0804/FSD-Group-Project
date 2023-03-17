a2c2c5b543c66b2f0e73161bcb729aa41e412388
Chandra0804 <chandrasaiteja0804@gmail.com>
2023-03-17 09:41:32 +0530

    Updated Tasks list

--- scripts/RoomViewPage.js
+++ scripts/RoomViewPage.js
@@ -55,20 +55,76 @@ stopBtn.addEventListener('click', stopTimer);
 
 const istasks = false;
 const isnotes = false;
-document.getElementById("tasks").onmousedown = function() {
-    if(isnotes===false){
-        document.getElementById("tasks-wrapper").style.display = "block";
-        document.getElementById("notes-wrapper").style.display = "none";
-        istasks = true;
-        isnotes = false;
-    }
+document.getElementById("tasks").onmousedown = function () {
+  if (isnotes === false) {
+    document.getElementById("tasks-wrapper").style.display = "block";
+    document.getElementById("notes-wrapper").style.display = "none";
+    istasks = true;
+  }
 }
 
-document.getElementById("notes").onmousedown = function() {
-    if(istasks===false){
-        document.getElementById("notes-wrapper").style.display = "block";
-        document.getElementById("tasks-wrapper").style.display = "none";
-        isnotes = true;
-        istasks = false;
+document.getElementById("notes").onmousedown = function () {
+  if (istasks === false) {
+    document.getElementById("notes-wrapper").style.display = "block";
+    document.getElementById("tasks-wrapper").style.display = "none";
+    isnotes = true;
+  }
+}
+
+const isassignments = false;
+const isrsources = false;
+document.getElementById("resources").onmousedown = function () {
+  if (isassignments === false) {
+    document.getElementById("resources-wrapper").style.display = "block";
+    document.getElementById("assignments-wrapper").style.display = "none";
+    isrsources = true;
+  }
+}
+
+document.getElementById("assignments").onmousedown = function () {
+  if (isnotes === false) {
+    document.getElementById("assignments-wrapper").style.display = "block";
+    document.getElementById("resources-wrapper").style.display = "none";
+    isassignments = true;
+  }
+}
+
+const submitForm = document.querySelector('.add');
+const addButton = document.querySelector('.add-todo');
+const todoList = document.querySelector('.todos');
+const list = document.querySelectorAll('.todos li'); //gives a NodeList of all li's
+let listLength = list.length;
+
+const generateTemplate = (todo) => {
+
+    const html = `<li>
+                    <input type="checkbox" id="todo_${listLength}" />
+                    <label for="todo_${listLength}">
+                        <span class="check"></span>${todo}
+                    </label>
+                    <i class="far fa-trash-alt delete"></i>
+                  </li>`
+    todoList.innerHTML += html;
+};
+
+function addTodos(e) {
+    e.preventDefault();
+    const todo = submitForm.add.value.trim(); //trim extra space in input
+    if (todo.length) { // to check whether the input is empty
+        listLength = listLength + 1; // to update the checkbox id when we add a new list item
+        generateTemplate(todo);
+        submitForm.reset();
+    }
+
+}
+//add todos
+submitForm.addEventListener('submit', addTodos);
+addButton.addEventListener('click', addTodos);
+
+function deleteTodos(e) {
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
     }
 }
+//delete todos
+todoList.addEventListener('click', deleteTodos);
