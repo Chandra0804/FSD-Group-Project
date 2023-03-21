88c9415083ea48eaaa4e5eee0341e96848a3d9a8
Chandra0804 <chandrasaiteja0804@gmail.com>
2023-03-21 11:41:32 +0530

    Updated responsiveness for Room :

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static("public"))
 
-app.get('/login',(req,res)=>{
+app.get('/',(req,res)=>{
     res.render('Login_SignUp')
 })
 
-app.get('/',(req,res)=>{
+app.get('/room',(req,res)=>{
     res.render('RoomViewPage')
+})
+
+app.get('/mentorApplication',(req,res)=>{
+    res.render('mentorApplication')
 })
 
 app.listen(3000)
