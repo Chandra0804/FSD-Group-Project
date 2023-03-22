a27c3063b45fca2c87a56eb5a48b45bae00d3168
hariprasad2512 <111779312+hariprasad2512@users.noreply.github.com>
2023-03-22 19:00:41 +0530

    Update navbar.js

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -7,3 +7,13 @@ function loadLoginPage() {
     // Load the login page into the current window
     window.location.href = "/contactus";
   }
+
+  function loadAboutusPage(){
+    // Load the About Us Page into the current window
+    window.location.href = "/about";
+  }
+
+  function loadHomePage(){
+    //Load the Home Page into the current window
+    window.location.href = "/";
+  }
